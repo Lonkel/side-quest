212e6731f4cecad93dbdece4f08dbbf7cae96db3
Lonkel <linuscollin23@gmail.com>
2026-01-09 10:39:19 +0100

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+// supabase.js
+const SUPABASE_URL = 'https://sazcythsqpynnsifndja.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_DIoKeeg_btbba8BuoEYN5A_f4wo6MWL';
+
+const { createClient } = supabase;
+const db = createClient(SUPABASE_URL, SUPABASE_KEY);
 
