08d71796f6d67cb00436945f34b643d087290296
Lonkel <linuscollin23@gmail.com>
2026-06-17 09:26:26 +0200

    Remove debug log for expense dates
    Removed debug logging for all expense dates.

--- report.js
+++ report.js
@@ -597,12 +597,6 @@ async function loadExpenses() {
     console.log('DEBUG loadExpenses rows:', data);
 
     expenses = data || [];
-    console.log(
-      'DEBUG all expense dates:',
-      expenses
-        .map(e => `${e.id}:${e.date}`)
-        .sort()
-    );
     renderTable();
     updateSummary();
     renderYearTabs();
